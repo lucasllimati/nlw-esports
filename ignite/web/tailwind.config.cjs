7126a2fb36354f9a920a327d1c4566c6730e1793
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-09-18 17:25:03 -0300

    font inter

--- ignite/web/tailwind.config.cjs
+++ ignite/web/tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
